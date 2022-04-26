5885a818fd71159db12573e8bc28d3d0b00b6e72
Software Engineer <engineersoftware48@gmail.com>
2022-04-26 09:50:38 -0600

    server added

--- waker.js
+++ waker.js
@@ -1,22 +1,25 @@
-const https = require('https');
-// https://salvation1.herokuapp.com/
-const options = {
-  hostname: 'salvation1.herokuapp.com',
-  port: 443,
-  path: '/',
-  method: 'GET',
-};
-
-const req = https.request(options, res => {
-  console.log(`statusCode: ${res.statusCode}`);
-
-  res.on('data', d => {
-    process.stdout.write(d);
-  });
-});
-
-req.on('error', error => {
-  console.error(error);
-});
-
-req.end();
+function wakeup() {
+    const https = require('https');
+    // https://salvation1.herokuapp.com/
+    const options = {
+      hostname: 'salvation1.herokuapp.com',
+      port: 8080,
+      path: '/',
+      method: 'GET',
+    };
+    
+    const req = https.request(options, res => {
+      console.log(`statusCode: ${res.statusCode}`);
+    
+      res.on('data', d => {
+        process.stdout.write(d);
+      });
+    });
+    
+    req.on('error', error => {
+      console.error(error);
+    });
+    
+    req.end();
+}
+wakeup();
